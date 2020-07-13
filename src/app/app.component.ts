2ae20641f5e2b8ee4d7cdb646a32e2e29f8ddb7f
Vinit Kanani <kananivinit97@gmail.com>
2020-07-13 22:18:20 +0530

    Starter Setup Done :white_check_mark:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NbMenuItem, NbSidebarService} from '@nebular/theme';
+
+interface CardSettings {
+  title: string;
+  iconClass: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'dashboard-starter';
+  items: NbMenuItem[] = [
+    {
+      title: 'Profile',
+      icon: 'person-outline',
+    },
+    {
+      title: 'Change Password',
+      icon: 'lock-outline',
+    },
+    {
+      title: 'Privacy Policy',
+      icon: {icon: 'checkmark-outline', pack: 'eva'},
+    },
+    {
+      title: 'Logout',
+      icon: 'unlock-outline',
+    },
+  ];
+  menu: NbMenuItem[] = [
+    {
+      title: 'Vehicle',
+      icon: 'home-outline',
+      link: '/pages/dashboard',
+      home: true,
+      children: [
+        {
+          title: 'Form Inputs',
+          link: '/pages/forms/inputs',
+        },
+        {
+          title: 'Form Layouts',
+          link: '/pages/forms/layouts',
+        },
+        {
+          title: 'Buttons',
+          link: '/pages/forms/buttons',
+        },
+        {
+          title: 'Datepicker',
+          link: '/pages/forms/datepicker',
+        },
+      ]
+    },
+    {
+      title: 'Vehicle',
+      icon: 'home-outline',
+      link: '/pages/dashboard',
+      home: true,
+    },
+    {
+      title: 'Vehicle',
+      icon: 'home-outline',
+      link: '/pages/dashboard',
+      home: true,
+    }
+  ];
+
+  lightCard: CardSettings = {
+    title: 'Light',
+    iconClass: 'nb-lightbulb',
+    type: 'primary',
+  };
+
+  rollerShadesCard: CardSettings = {
+    title: 'Roller Shades',
+    iconClass: 'nb-roller-shades',
+    type: 'success',
+  };
+
+  wirelessAudioCard: CardSettings = {
+    title: 'Wireless Audio',
+    iconClass: 'nb-audio',
+    type: 'info',
+  };
+
+  coffeeMakerCard: CardSettings = {
+    title: 'Coffee Maker',
+    iconClass: 'nb-coffee-maker',
+    type: 'warning',
+  };
+
+  statusCards: CardSettings[] = [this.lightCard, this.coffeeMakerCard, this.wirelessAudioCard, this.rollerShadesCard];
+
+  constructor(private sidebarService: NbSidebarService) {
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true);
+
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbButtonModule,
+  NbMenuModule,
+  NbIconModule,
+  NbActionsModule,
+  NbUserModule, NbCardModule, NbInputModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { RouterModule } from '@angular/router';
+import { StatusCardComponent } from './dashboard/status-card/status-card.component';
+import { FormsComponent } from './dashboard/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusCardComponent,
+    FormsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'default'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbMenuModule.forRoot(),
+    NbButtonModule,
+    NbIconModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCardModule,
+    NbInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
